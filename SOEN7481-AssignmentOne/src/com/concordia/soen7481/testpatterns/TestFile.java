e1e4c9112e6d925fbb305d211cf1bf09739cd859
ariesabhi55 <aries.abhi5@gmail.com>
2019-10-06 15:28:29 -0400

    Main test file added with Junit test for 'equals without hashcode'

---
+++ SOEN7481-AssignmentOne/src/com/concordia/soen7481/testpatterns/TestFile.java
@@ -0,0 +1,31 @@
+package com.concordia.soen7481.testpatterns;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.concordia.soen7481.*;
+
+class TestFile {
+
+	String pathName = new File("").getAbsolutePath()+"\\src\\com\\concordia\\soen7481\\testpatterns";
+	
+	@BeforeEach public void initialize() {
+		StaticHelper.SourceFolder = pathName;
+		CodeParser codeParser = new CodeParser();
+        codeParser.parseProject();
+		}	
+	
+	@Test
+	void testEqualsWithoutHashcode() {
+		assertEquals(1, StaticHelper.getResults().get("com.concordia.soen7481.EqualsWithoutHashcodePattern").size());		
+		assertEquals(pathName+"\\EqualsWithoutHashcodePatternTestFail.java", StaticHelper.getResults().get("com.concordia.soen7481.EqualsWithoutHashcodePattern").get(0).getFileName());
+	}
+	
+
+}
